048a739abaa61a0c24da6ccda16cac65c6036873
Jane Baldwin <jbaldwin@berimbau.ldeo.columbia.edu>
2023-01-23 02:44:03 -0500

    removed WELLBEING

--- WELLBEING/gRIMM-UR2018/progress_reporter.py
+++
@@ -1,7 +0,0 @@
-import sys
-from IPython.display import clear_output
-def progress_reporter(c):
-    #report progress
-    clear_output()
-    print("Currently working on: ", c)
-    sys.stdout.flush()    
